e7270c283ea81d6ceac3dce6286522ecf13d996a
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-12-27 02:19:10 +0300

    Update Single_responsibility_principle.py

--- Single_responsibility_principle.py
+++ Single_responsibility_principle.py
@@ -14,16 +14,23 @@ class TelephoneDirectory:
   def lookup_number(self, name):
     return self.telephonedirectory[name]
  
-  def save_to_file(self, file_name, location):
-    #code to save the contents of telephonedirectory dictionary to the file
-    pass
+#   def save_to_file(self, file_name, location):
+#     pass
 
-  def persist_to_database(self, database_details):
-    #code to persist the contents of telephonedirectory dictionary to database
-    pass
+#   def persist_to_database(self, database_details):
+#     pass
 
   def __str__(self):
     ret_dct = ""
     for key, value in self.telephonedirectory.items():
       ret_dct += f'{key} : {value}\n'
     return ret_dct
+  
+class persist_to_database:
+  def __init__(self, object_to_persist):
+    pass
+
+class save_to_file:
+  def __init__(self, object_to_save):
+    pass
+  
